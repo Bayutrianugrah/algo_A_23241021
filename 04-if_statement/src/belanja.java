2db995dec428672173e1642f21855b9c3a8e1bcd
Bayutrianugrah <145947008+Bayutrianugrah@users.noreply.github.com>
2023-12-29 11:14:00 +0800

    tugas tgl 29 desember

---
+++ 04-if_statement/src/belanja.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class belanja {
+
+    public static void main(String[] args) {
+        //data member atau variabel
+        int tot_belanja;
+
+        //membuat objek scanner
+        Scanner input = new Scanner(System.in);
+        System.out.print("Masukan Total Belanja :" );
+        tot_belanja = input.nextInt();
+
+        //membuat percabangan
+        if (tot_belanja >= 50000){
+            System.out.println("Selamat Dapat Mouse");
+            } else {
+                System.out.println("Tidak Dapsat MOUSE");
+            }
+
+        }
+    }
+    
